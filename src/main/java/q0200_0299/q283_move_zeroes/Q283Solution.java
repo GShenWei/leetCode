9d7837052388f867cee7ea5f409f00e26697a6c2
chenwei <741154666@qq.com>
2020-05-13 21:54:02 +0800

    maven

--- src/main/java/q0200_0299/q283_move_zeroes/Q283Solution.java
+++ src/main/java/q0200_0299/q283_move_zeroes/Q283Solution.java
@@ -19,7 +19,33 @@ public class Q283Solution {
     @Test
     public void xx() {
         int[] nums = {0, 1, 0, 3, 12};
-        moveZeroes(nums);
+        moveZeroes3(nums);
         System.out.println(Arrays.toString(nums));
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                nums[p++] = nums[i];
+            }
+        }
+        for (int i = p; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public void moveZeroes3(int[] nums) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                if (i != p) {
+                    int t = nums[i];
+                    nums[i] = nums[p];
+                    nums[p] = t;
+                }
+                p++;
+            }
+        }
     }
 }
