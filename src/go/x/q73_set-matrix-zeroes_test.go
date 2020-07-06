f68f0b11625d4f4e94971458688484d0a848ee90
weichen <741154666@qq.com>
2020-07-06 16:16:59 +0800

    gohaha

---
+++ src/go/x/q73_set-matrix-zeroes_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGo2(t *testing.T) {
+	//x := [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}
+	x := [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
+	//setZeroes(x)
+	gl(setZeroes, x)
+	fmt.Println(x)
+}
+
+func setZeroes(matrix [][]int) {
+	spe := -2 << 31
+	for i := range matrix {
+		for j := range matrix[i] {
+			if matrix[i][j] == 0 {
+				for k := 0; k < len(matrix[i]); k++ {
+					if matrix[i][k] != 0 {
+						matrix[i][k] = spe
+					}
+				}
+				for k := 0; k < len(matrix); k++ {
+					if matrix[k][j] != 0 {
+						matrix[k][j] = spe
+					}
+				}
+			}
+		}
+	}
+	for i := range matrix {
+		for j := range matrix[i] {
+			if matrix[i][j] == spe {
+				matrix[i][j] = 0
+			}
+		}
+	}
+}
+
+func TestPlo(t *testing.T) {
+	var x []int
+	ints := append(x, 90)
+	fmt.Println(ints)
+
+	xx := make([]int, 20)
+	for i := range xx {
+		xx[i] = i
+	}
+	fmt.Println(xx)
+
+	gg := xx[5:6]
+	fmt.Println(gg)
+
+	var nn [][]int
+	var c = append(nn, xx[5:9])
+	c = append(c, xx[6:9])
+	fmt.Println(c)
+}
+
+type Pi interface {
+}
+
+type Po interface {
+}
+
+func TestPl(t *testing.T) {
+	pis := make([]Pi, 2)
+	var xx = append(pis, new(Po))
+	fmt.Println(xx)
+}
+
+func gl(f func([][]int), matrix [][]int) {
+	f(matrix)
+}
---
+++ src/go/x/q918_maximum-sum-circular-subarray_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGo(t *testing.T) {
+	x := []int{3, -2, 5}
+	fmt.Println(maxSubarraySumCircular(x))
+}
+
+func maxSubarraySumCircular(A []int) int {
+	max, maxNow := -2<<31, -2<<31
+	min, minNow := 2<<31, 2<<31
+	maxVal := -2 << 31
+	total := 0
+	for i := 0; i < len(A); i++ {
+		total += A[i]
+		maxVal = Max(maxVal, A[i])
+
+		maxNow = Max(A[i], maxNow+A[i])
+		max = Max(max, maxNow)
+
+		minNow = Min(A[i], minNow+A[i])
+		min = Min(min, minNow)
+	}
+	if maxVal <= 0 {
+		return maxVal
+	}
+	return Max(max, total-min)
+}
+
+func Max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func Min(a int, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
