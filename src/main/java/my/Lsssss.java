1656582933c39dc1dc2d6ad45be3da19f4afcc41
chenwei <741154666@qq.com>
2022-07-24 00:03:48 +0800

    调整到17

--- src/main/java/my/Lsssss.java
+++ src/main/java/my/Lsssss.java
@@ -1,5 +1,9 @@
 package my;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,21 +13,20 @@ public class Lsssss {
     @Test
     public void xx() {
         List<Student> aa = new ArrayList<>();
-        aa.add(new Student(1,"wang"));
-        aa.add(new Student(2,"ming"));
-        aa.add(new Student(3,"hong"));
-        aa.add(new Student(4,"le"));
-        aa.add(new Student(5,"qifa"));
+        aa.add(new Student(1, "wang"));
+        aa.add(new Student(2, "ming"));
+        aa.add(new Student(3, "hong"));
+        aa.add(new Student(4, "le"));
+        aa.add(new Student(5, "qifa"));
+        System.out.println(aa.get(0));
     }
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     class Student {
-        public Student(Integer age, String name) {
-            this.age = age;
-            this.name = name;
-        }
-
-        Integer age;
-        String name;
+        private Integer age;
+        private String name;
     }
 }
 
